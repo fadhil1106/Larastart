4250c1d03450083fa2a7117f9be4d8d4752573d4
Fuad Fadhil Azzar <fuadf.512@gmail.com>
2019-10-26 18:33:37 +0800

    install adminlte 3 and bootstrap 4

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://larastart.test'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
