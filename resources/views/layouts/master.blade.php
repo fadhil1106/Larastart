b88cfd866b299bd7221870aa6acc2289d1de9c77
Fuad Fadhil Azzar <fuadf.512@gmail.com>
2019-12-13 11:13:00 +0800

    adding laravel passport secure url with jwt

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -100,6 +100,15 @@
             </li>
 
             <li class="nav-item">
+              <router-link to="/developer" class="nav-link">
+                <i class="nav-icon fas fa-cogs"></i>
+                <p>
+                  Developer
+                </p>
+              </router-link>
+            </li>
+            
+            <li class="nav-item">
               <router-link to="/profile" class="nav-link">
                 <i class="nav-icon fas fa-user orange"></i>
                 <p>
@@ -107,6 +116,7 @@
                 </p>
               </router-link>
             </li>
+
             <li class="nav-item">
               <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
